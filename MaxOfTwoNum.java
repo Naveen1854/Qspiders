129003cd43b5c3110bf98fca00c07e9d376ef157
Naveen1854 <kusuma.naveen1854@gmail.com>
2024-08-08 12:38:30 +0530

    commit

---
+++ MaxOfTwoNum.java
@@ -0,0 +1,13 @@
+class  MaxOfTwoNum
+{
+	public static void main(String[] args) 
+	{
+		int a = 54;
+		int b = 100;
+		if(a>b){
+			System.out.println(a+ " is max");
+		}else{
+			System.out.println(b+ " is max");
+		}
+	}
+}
---
+++ PositiveOrNegative.java
@@ -0,0 +1,16 @@
+class PositiveOrNegative 
+{
+	public static void main(String[] args) 
+	{
+		int a = 54;
+		if(a>0){
+			System.out.println(a+ " is positive");
+		}
+		else if(a<0){
+			System.out.println(a+ " is Negative");
+		}
+		else{
+			System.out.println(a+ " is Zero");
+		}
+	}
+}
