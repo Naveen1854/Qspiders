aaa71bb53e7501aad5522ff9476ec8fdef5ee455
Naveen1854 <kusuma.naveen1854@gmail.com>
2024-08-10 20:16:12 +0530

    day_2

---
+++ SwitchDemo.java
@@ -0,0 +1,18 @@
+class SwitchDemo 
+{
+	public static void main(String[] args) 
+	{
+		int  a = 4;
+		switch (a){
+		case 5: System.out.println("Akash");
+		break;
+		case 4: System.out.println("Naveen");
+					System.out.println("Soft Engineer");
+					System.out.println("Full Stack Developer");
+					break;
+		case 6:System.out.println("Nithin");
+		break;
+		default : System.out.println("Invalid Input");
+		}
+	}
+}
---
+++ SwitchDemo1java.java
@@ -0,0 +1,20 @@
+class SwitchDemo1  
+{
+	public static void main(String[] args) 
+	{
+		char c = 'A';
+		switch (c)
+		{
+		case 'A':System.out.println(c+" is vowel");
+		break;
+		case 'E':System.out.println(c+" is vowel");
+		break;
+		case 'I':System.out.println(c+" is vowel");
+		break;
+		case 'O':System.out.println(c+" is vowel");
+		break;
+		case 'U':System.out.println(c+" is vowel");
+		default :System.out.println("Not Vowel");
+		}
+	}
+}
