c6db9e8f21d6b1ec144552ae25b18aecc27f5e42
Naveen1854 <kusuma.naveen1854@gmail.com>
2024-08-21 18:42:31 +0530

    day5

---
+++ Patterns/Pattern1.java
@@ -0,0 +1,18 @@
+class  Pattern1
+{
+	public static void main(String[] args)
+	{
+		for (int line=1; line<=4; line++)
+		{
+			for (int space=3; space>=line; space--)
+			{
+				System.out.print(" ");
+			}
+			for (int ast  = 1;ast <=line ;ast++ )
+			{
+				System.out.print("* ");
+			}
+				System.out.println();
+		}
+	}
+}
